e84333d3c8b23c022243a0b9f22f68d07787a0db
Karthik7991 <karanam@iiserb.ac.in>
2018-07-13 23:16:30 +0530

    we wrote if else programm in python

--- greeting.py
+++ greeting.py
@@ -1,3 +1,10 @@
-x=input("enter your name:")
+x=int(input("enter your number:"))
 print(x)
+if (x>0):
+	print("its a positive positive")
+if (x<0):
+    print("its a negative number")
+else:
+    print("it is zero")    
+    	
 
